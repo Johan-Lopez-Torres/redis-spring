f62697638bc13d0627d36b24db2b786f4ba6611e
Johan Lopez <johanpppp222345@gmail.com>
2024-10-15 08:39:31 -0500

    codebase

---
+++ src/main/java/org/johan/microservices/paypalwebflux/PaypalWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.johan.microservices.paypalwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/johan/microservices/paypalwebflux/PaypalWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.johan.microservices.paypalwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
